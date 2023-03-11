2d913402a79a9c641ccb14e5f0274c0cce246f74
shivarajkumar <shivaraj1654@gmail.com>
2023-03-11 13:35:26 +0530

    Sent mail

--- src/Components/Mail/MailData.js
+++ src/Components/Mail/MailData.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { useDispatch } from 'react-redux'
 import { Container , Row , Col , Button } from 'react-bootstrap';
 import deleteMail from '../../store/MailAction';
+import { NavLink } from 'react-router-dom'
 
 const MailData = (props) => {
   const dispatch = useDispatch();
@@ -12,14 +13,16 @@ const MailData = (props) => {
     <Fragment>
     <Container>
         <Row>
+        <NavLink to={`/${props.mail.id}`}>
         { props.toorFrom==="From" && !props.mail.read &&
           <Col xs={1}>
             <iconify-icon icon="ri:checkbox-blank-circle-fill" style={{color : "blue"}}></iconify-icon>
           </Col>}
           <Col xs={1}>{props.toorFrom}</Col>
           <Col xs={3}>
-            <b>{props.mail.from}</b>
+          <b>{(props.toorFrom=== 'From')?props.mail.from:props.mail.to}</b>
           </Col>
+          </NavLink>
           <Col xs={5}>{props.mail.title}</Col>
           <Col xs={2}>
             <Button variant="danger" onClick={deleteMailHandler}>Delete</Button>
